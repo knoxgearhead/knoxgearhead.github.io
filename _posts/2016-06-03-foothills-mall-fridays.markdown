a2f9c7973447203f1d0c71c6de462bb6c4eee1bf
Steven R. Young <stevenryoung@gmail.com>
2016-01-05 22:39:21 -0500

    Added support for weekly events.

---
+++ _posts/2016-06-03-foothills-mall-fridays.markdown
@@ -0,0 +1,17 @@
+---
+layout: event
+title:  "Foothills Mall Cruise-In"
+date:   2016-06-03 22:36:59
+categories: carshow
+location: "Foothills Mall"
+address: "197 Foothills Mall Dr, Maryville, TN 37801"
+website: "http://www.smokymountainstreetrodders.com/upcoming-events.html"
+rec_start: 2016-04-01 00:00:00
+rec_end: 2016-10-31 20:00:00
+rec_period: weekly
+rec_day: friday
+---
+
+Every Friday April through September at Foothills Mall in Maryville.
+
+Hosted by the Smoky Mountain Street Rodders.
---
+++ scripts/generate_recurring_events.py
@@ -0,0 +1,40 @@
+import dateutil.rrule as dr
+import dateutil.parser as dp
+import dateutil.relativedelta as drel
+from glob import glob
+import yaml
+
+DAYS = {"monday": dr.MO,
+        "tuesday":dr.TU,
+        "wednesday":dr.WE,
+        "thursday":dr.TH,
+        "friday":dr.FR,
+        "saturday":dr.SA,
+        "sunday":dr.SU}
+
+for eventfile in glob("_recurring/*.markdown"):
+
+    with open(eventfile, 'r') as txt:
+        content = txt.read()
+
+    info = yaml.load(content.split("---")[1])
+
+    # start = dp.parse(info['rec_start'])
+    # end = dp.parse(info['rec_end'])
+
+    start = info['rec_start']
+    end = info['rec_end']
+
+    if "week" in info['rec_period']:
+        rr = dr.rrule(dr.WEEKLY,byweekday=DAYS[info['rec_day']])
+    else:
+        print "Error: this recurring type not supported"
+
+    dates = rr.between(start, end)
+
+    for d in dates:
+        formatted_date = str(d)
+        c = content.replace("DATE", formatted_date)
+        filename = "_posts/" + formatted_date.split(" ")[0] + '-' + eventfile.split("/")[-1]
+        with open(filename, "w") as outfile:
+            outfile.write(c)
