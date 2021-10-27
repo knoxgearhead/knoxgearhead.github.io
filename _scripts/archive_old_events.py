d6ccba9a69bbc4982a1a5d1053e9399f1a6f785b
Steven R. Young <stevenryoung@gmail.com>
2021-10-26 22:20:35 -0400

    Update scripts

--- _scripts/archive_old_events.py
+++ _scripts/archive_old_events.py
@@ -4,20 +4,23 @@ from glob import glob
 import os
 import yaml
 
-
+# Iterate over all events in the events directory
 for eventfile in glob("_posts/*.markdown"):
 
+    # Load the event file
     with open(eventfile, 'r') as txt:
         content = txt.read()
 
+    # Load the YAML frontmatter
     info = yaml.safe_load(content.split("---")[1])
 
-
+    # Get the date of the event
     if 'end_date' in info:
         d = info['end_date']
     else:
         d = info['date']
 
+    # Move the event to the archive directory if occured greater than a day ago
     if datetime.now() > ( d + drel.relativedelta(days=1) ):
         print("Archived: {}".format(eventfile))
         os.rename(eventfile, eventfile.replace("_posts", "_archive"))
--- _scripts/build_calendars.py
+++ _scripts/build_calendars.py
@@ -1,4 +1,3 @@
-from datetime import datetime, timedelta
 import dateutil.relativedelta as drel
 import icalendar as cal
 from glob import glob
@@ -7,32 +6,45 @@ import yaml
 
 
 
-# ALL EVENTS
+# Build the calendar for all events
 c = cal.Calendar()
-
 c.add('prodid', '-//KnoxGearhead Calendar//knoxgearhead.com//')
 c.add('version', '0.0.1')
 
+# Build the calendar for drag racing events
+c_drag = cal.Calendar()
+c_drag.add('prodid', '-//KnoxGearhead Carshow and Cruise-In Calendar//knoxgearhead.com//')
+c_drag.add('version', '0.0.1')
+
+# Build the calendar for car shows and cruise ins
+c_showcruise = cal.Calendar()
+c_showcruise.add('prodid', '-//KnoxGearhead Drag Racing Calendar//knoxgearhead.com//')
+c_showcruise.add('version', '0.0.1')
+
+# Iterate over all events
 for f in glob('_posts/*.markdown'):
+    # Create a new event
     e = cal.Event()
 
+    # Load the event data
     with open(f, 'r') as txtfile:
         txt = txtfile.read()
-
     info = yaml.safe_load(txt.split('---')[1])
 
+    # Add the event data    
     e.add('summary', info['title'])
     e.add('dtstart', info['date'].replace(tzinfo=pytz.timezone('US/Eastern')))
 
+    # Calculate the end date
     if 'end_date' in info:
         end_time = info['end_date']
     elif 'rec_end' in info:
         end_time = info['date'] + drel.relativedelta(hour=info['rec_end'].hour)
     else:
         end_time = info['date'] + drel.relativedelta(hours=4)
-
     e.add('dtend', end_time.replace(tzinfo=(pytz.timezone('US/Eastern'))))
 
+    # Add metadata to the event
     organizer = cal.vCalAddress('MAILTO:knoxgearhead@knoxgearhead.com')
     organizer.params['cn'] = cal.vText('KnoxGearhead')
     e.add('organizer', organizer)
@@ -40,13 +52,22 @@ for f in glob('_posts/*.markdown'):
     e['location'] = cal.vText(info['address'])
     e['description'] = cal.vText('http://www.knoxgearhead.com \nWebsite: {}\n Location: {}\n\n{}'.format(info['website'], info['location'], txt.split('---')[-1]))
 
+    # Add the event to the calendar(s)
     c.add_component(e)
+    if "drag" in info['categories']:
+        c_drag.add_component(e)
+    elif "carshow" in info['categories'] or "cruisein" in info['categories']:
+        c_showcruise.add_component(e)
 
-
+# Write the calendars to files
 with open('calendars/all_events.ics', 'wb') as f:
     f.write(c.to_ical())
-    
-    
+with open('calendars/drag_races.ics', 'wb') as f:
+    f.write(c_drag.to_ical())
+with open('calendars/carshows.ics', 'wb') as f:
+    f.write(c_showcruise.to_ical())
+
+exit() 
 # CAR SHOWS
 c = cal.Calendar()
 
