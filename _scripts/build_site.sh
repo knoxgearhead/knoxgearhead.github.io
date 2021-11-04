2b3db8921e5c0b05f0fdb642363511cd05b30276
Steven R. Young <stevenryoung@gmail.com>
2021-11-03 22:58:48 -0400

    Trying to add github action

--- _scripts/build_site.sh
+++ _scripts/build_site.sh
@@ -6,5 +6,5 @@ git config --global user.name 'Steven R. Young'
 git config --global user.email 'stevenryoung@gmail.com'
 git add _posts/*
 git add _archive/*
-git commit -m "Automated Site Build" || echo "No changes to commit"
+git commit -am "Automated Site Build" || echo "No changes to commit"
 git push origin master || echo "No changes to push"
